378fc6de901bb45be0bdbfd9fe11f876458d495e
jeremy <jeremy@blend.com>
2019-07-25 15:23:04 -0700

    update GithubClient to handle enterprise hostnames

--- src/server/plugin/Plugin.ts
+++ src/server/plugin/Plugin.ts
@@ -35,7 +35,10 @@ function log(...args: any[]) {
  */
 export default class GithubOauthUiPlugin implements MiddlewarePlugin, AuthPlugin {
 
-  private readonly github = new GithubClient(this.config.user_agent)
+  private readonly github = new GithubClient(this.config.user_agent,
+    this.config["is-github-enterprise"],
+    this.config.org,
+  )
   private readonly cache: { [username: string]: UserDetails } = {}
   private readonly cliSupport = new SinopiaGithubOAuthCliSupport(this.config, this.stuff)
 
