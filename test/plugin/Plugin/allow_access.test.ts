4dccf366bd761b8723c87fe4c8091668e68c0c17
Abraham Schilling <AbrahamSchilling@gmail.com>
2019-11-01 17:44:18 +0100

    Add access tests

---
+++ test/plugin/Plugin/allow_access.test.ts
@@ -0,0 +1,91 @@
+import { PackageAccess, RemoteUser } from "@verdaccio/types"
+import { PluginConfigProps } from "src/server/plugin/Config"
+import { GithubOauthUiPlugin } from "src/server/plugin/Plugin"
+
+import { createTestConfig, createTestPlugin } from "../test-utils"
+
+const authenticated = "$authenticated"
+
+describe("Plugin", () => {
+  describe("allow_access", () => {
+
+    let config: PluginConfigProps
+    let plugin: GithubOauthUiPlugin
+
+    beforeEach(() => {
+      config = createTestConfig()
+      plugin = createTestPlugin()
+    })
+
+    it("unauthenticated user does not have access", (done) => {
+      const user: RemoteUser = {
+        real_groups: [],
+        groups: [],
+        name: "test-package",
+      }
+      const pkg: PackageAccess = {
+        access: [authenticated],
+      }
+
+      plugin.allow_access(user, pkg, (err, groups) => {
+        expect(err).toBeNull()
+        expect(groups).toBe(false)
+        done()
+      })
+    })
+
+    it("authenticated user outside org does not have access", (done) => {
+      const userGroups = [authenticated]
+      const user: RemoteUser = {
+        real_groups: userGroups,
+        groups: userGroups,
+        name: "test-package",
+      }
+      const pkg: PackageAccess = {
+        access: [authenticated],
+      }
+
+      plugin.allow_access(user, pkg, (err, groups) => {
+        expect(err).toBeNull()
+        expect(groups).toBe(false)
+        done()
+      })
+    })
+
+    it("authenticated user in org has access", (done) => {
+      const userGroups = [authenticated, config.org]
+      const user: RemoteUser = {
+        real_groups: userGroups,
+        groups: userGroups,
+        name: "test-package",
+      }
+      const pkg: PackageAccess = {
+        access: [authenticated],
+      }
+
+      plugin.allow_access(user, pkg, (err, groups) => {
+        expect(err).toBeNull()
+        expect(groups).toEqual(userGroups)
+        done()
+      })
+    })
+
+    it("authenticated user has access if authentication is not required", (done) => {
+      const user: RemoteUser = {
+        real_groups: [],
+        groups: [],
+        name: "test-package",
+      }
+      const pkg: PackageAccess = {
+        access: [],
+      }
+
+      plugin.allow_access(user, pkg, (err, groups) => {
+        expect(err).toBeNull()
+        expect(groups).toEqual([])
+        done()
+      })
+    })
+
+  })
+})
