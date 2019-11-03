141e157ae5e75edc62ae0a0df08cdbe943571a65
Abraham Schilling <AbrahamSchilling@gmail.com>
2019-11-03 12:07:39 +0100

    Review lgtm findings

--- src/cli/npm.ts
+++ src/cli/npm.ts
@@ -1,6 +1,6 @@
 import { execSync } from "child_process"
 import minimist from "minimist"
-import { parse } from "url"
+import { URL } from "url"
 
 export const npmConfig = JSON.parse(execSync("npm config list --json").toString())
 const args = minimist(process.argv.slice(2))
@@ -9,7 +9,7 @@ export const registry = (args.registry || npmConfig.registry).trim()
   .replace(/\/?$/, "") // Remove potential trailing slash
 
 export function saveToken(token: string) {
-  const url = parse(registry)
-  execSync(`npm config set //${url.host! + url.path!}:_authToken "${token}"`)
-  execSync(`npm config set //${url.host! + url.path!}:always-auth true`)
+  const url = new URL(registry)
+  execSync(`npm config set //${url.host + url.pathname}:_authToken "${token}"`) // lgtm [js/command-line-injection]
+  execSync(`npm config set //${url.host + url.pathname}:always-auth true`)
 }
