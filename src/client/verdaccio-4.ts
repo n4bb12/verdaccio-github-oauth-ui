eac5f7a7a938e1de575c9ae23c72811a6a0b429b
Michael Mok <pmmmwh@gmail.com>
2019-11-18 19:21:07 +0800

    Update logic for the dialog box to support more package managers with better layout

--- src/client/verdaccio-4.ts
+++ src/client/verdaccio-4.ts
@@ -1,7 +1,6 @@
 import { getUsageInfo, init, isLoggedIn } from "./plugin"
 
 const usageInfoSelector = "#help-card .MuiCardContent-root span, .MuiDialogContent-root .MuiTypography-root span"
-const randomId = "Os1waV6BSoZQKfFwNlIwS"
 
 // copied from here as it needs to be the same behaviour
 // https://github.com/verdaccio/ui/blob/master/src/utils/cli-utils.ts
@@ -18,38 +17,56 @@ export function copyToClipboard(text: string) {
   document.body.removeChild(node)
 }
 
-function markUsageInfoNodes() {
-  document.querySelectorAll(usageInfoSelector).forEach(node => {
-    const infoEl = node as HTMLSpanElement
-
-    if (infoEl.innerText.includes("adduser")) {
-      infoEl.classList.add(randomId)
-    }
-  })
-}
-
 function modifyUsageInfoNodes() {
   const usageInfo = getUsageInfo()
   const loggedIn = isLoggedIn()
 
-  document.querySelectorAll("." + randomId).forEach(node => {
+  const infoElements = document.querySelectorAll(usageInfoSelector)
+  let packageManager: "npm" | "pnpm" | "yarn" | undefined
+  const cachedParent = Array.prototype.find.call(infoElements, node => {
+    const match = node.innerText.match(
+      // This checks for an element showing instructions to set the registry URL
+      /((npm|pnpm) set|(yarn) config set)/,
+    )
+    // When it matches, it can either be:
+    // 1. The second capture group: npm/pnpm
+    // 2. The third capture group: yarn
+    packageManager = match ? (match[1] || match[2]) : undefined
+    return !!match
+  }).parentElement as HTMLDivElement
+
+  infoElements.forEach((node => {
     const infoEl = node as HTMLSpanElement
-    const parentEl = infoEl.parentElement as HTMLDivElement
-    const copyEl = parentEl.querySelector("button") as HTMLButtonElement
-
-    infoEl.innerText = usageInfo
-
-    copyEl.style.visibility = loggedIn ? "visible" : "hidden"
-    copyEl.onclick = e => {
-      e.preventDefault()
-      e.stopPropagation()
-      copyToClipboard(usageInfo)
+    if (
+      // We only match lines related to bundler commands
+      infoEl.innerText.match(/^(npm|pnpm|yarn)/) &&
+      // And only commands that are shown in the popup overlay, to prevent overriding the help box
+      (infoEl.innerText.includes("adduser") || infoEl.innerText.includes("set password"))
+    ) {
+      infoEl.parentElement!.parentElement!.removeChild(node.parentElement as HTMLDivElement)
     }
-  })
+  }))
+
+  if (cachedParent) {
+    usageInfo.split("\n").forEach(info => {
+      const clonedNode = cachedParent.cloneNode(true) as HTMLDivElement
+      const textElem = clonedNode.querySelector("span") as HTMLSpanElement
+      const copyEl = clonedNode.querySelector("button") as HTMLButtonElement
+
+      // If cachedParent is defined, packageManager should be defined
+      textElem.innerText = packageManager ? info.replace(/^npm/, packageManager) : info
+
+      copyEl.style.visibility = loggedIn ? "visible" : "hidden"
+      copyEl.onclick = e => {
+        e.preventDefault()
+        e.stopPropagation()
+        copyToClipboard(info)
+      }
+    })
+  }
 }
 
 function updateUsageInfo() {
-  markUsageInfoNodes()
   modifyUsageInfoNodes()
 }
 
