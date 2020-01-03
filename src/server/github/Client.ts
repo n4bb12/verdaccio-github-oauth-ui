20e8342daaee808d7de3903528a140794bb13b24
Abraham Schilling <AbrahamSchilling@gmail.com>
2020-01-03 01:04:55 +0100

    Major dependency updates

--- src/server/github/Client.ts
+++ src/server/github/Client.ts
@@ -1,5 +1,4 @@
-import got, { GotJSONOptions } from "got"
-import { merge } from "lodash"
+import got from "got"
 
 import { GitHubOAuth } from "./OAuth"
 import { GitHubOrganization } from "./Organization"
@@ -17,16 +16,17 @@ export class GitHubClient {
    *
    * [Web application flow](bit.ly/2mNSppX).
    */
-  requestAccessToken = async (code: string, clientId: string, clientSecret: string) => {
+  requestAccessToken = async (code: string, clientId: string, clientSecret: string): Promise<GitHubOAuth> => {
     const url = this.webBaseUrl + "/login/oauth/access_token"
     const options = {
-      body: {
+      method: "POST",
+      json: {
         client_id: clientId,
         client_secret: clientSecret,
         code,
       },
-    }
-    return this.request<GitHubOAuth>(url, options)
+    } as const
+    return got(url, options).json()
   }
 
   /**
@@ -34,14 +34,14 @@ export class GitHubClient {
    *
    * [Get the authenticated user](https://developer.github.com/v3/users/#get-the-authenticated-user)
    */
-  requestUser = async (accessToken: string) => {
+  requestUser = async (accessToken: string): Promise<GitHubUser> => {
     const url = this.apiBaseUrl + "/user"
     const options = {
       headers: {
         Authorization: "Bearer " + accessToken,
       },
-    }
-    return this.request<GitHubUser>(url, options)
+    } as const
+    return got(url, options).json()
   }
 
   /**
@@ -49,20 +49,14 @@ export class GitHubClient {
    *
    * [List your organizations](https://developer.github.com/v3/orgs/#list-your-organizations)
    */
-  requestUserOrgs = async (accessToken: string) => {
+  requestUserOrgs = async (accessToken: string): Promise<GitHubOrganization[]> => {
     const url = this.apiBaseUrl + "/user/orgs"
     const options = {
       headers: {
         Authorization: "Bearer " + accessToken,
       },
-    }
-    return this.request<GitHubOrganization[]>(url, options)
-  }
-
-  private async request<T>(url: string, additionalOptions: Partial<GotJSONOptions>): Promise<T> {
-    const options = merge({ json: true }, additionalOptions)
-    const response = await got(url, options)
-    return response.body
+    } as const
+    return got(url, options).json()
   }
 
 }
