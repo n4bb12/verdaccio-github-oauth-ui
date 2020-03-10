5945402fdf2443bb59d15e422414959428681fa9
ganapativs <ganapati.vs@tracxn.com>
2020-03-10 17:59:11 +0530

    Move comment

--- src/cli/npm.ts
+++ src/cli/npm.ts
@@ -16,8 +16,9 @@ export function getConfigFile() {
 
 export function getSaveCommands(registry: string, token: string) {
   const url = new URL(registry)
+  // restore trailing slash if missing
   const pathname = url.pathname.endsWith("/") ? url.pathname : `${url.pathname}/`
-  const baseUrl = url.host +  pathname // restore trailing slash if missing
+  const baseUrl = url.host +  pathname
   return [
     `npm config set //${baseUrl}:_authToken "${token}"`, // lgtm [js/command-line-injection]
     `npm config set //${baseUrl}:always-auth true`,
