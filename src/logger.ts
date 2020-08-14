4a013b8e4d6525ebb75814b53fe602ea4cc1d401
n4bb12 <AbrahamSchilling@gmail.com>
2020-08-14 09:20:50 +0200

    Log the plugin version on startup

--- src/logger.ts
+++ src/logger.ts
@@ -9,3 +9,7 @@ export const logger = {
   log: console.log.bind(console, prefix),
   error: console.error.bind(console, prefix),
 }
+
+// tslint:disable-next-line: no-var-requires
+const plugin = require(__dirname + "/../package.json")
+logger.log(`Version: ${plugin.name}@${plugin.version}`)
