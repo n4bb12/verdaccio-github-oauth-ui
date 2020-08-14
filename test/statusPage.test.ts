4e06742ff86e9f61a3d552e206968656cdef6edd
n4bb12 <AbrahamSchilling@gmail.com>
2020-08-14 02:55:10 +0200

    Make CLI success page independent from verdaccio.org

--- test/statusPage.test.ts
+++ test/statusPage.test.ts
@@ -1,9 +1,21 @@
-import { buildStatusPage } from "src/statusPage"
+import {
+  accessDeniedPage,
+  buildErrorPage,
+  buildStatusPage,
+} from "src/statusPage"
+
+import { testErrorMessage } from "./utils"
 
 describe("Shared", () => {
   describe("buildStatusPage", () => {
-    it("matches the snapshot", () => {
+    it("generic page matches the snapshot", () => {
       expect(buildStatusPage("custom test body")).toMatchSnapshot()
+    })
+    it("error page matches the snapshot", () => {
+      expect(buildErrorPage(new Error(testErrorMessage))).toMatchSnapshot()
+    })
+    it("access denied page matches the snapshot", () => {
+      expect(accessDeniedPage).toMatchSnapshot()
     })
   })
 })
