59b81004abd42289b3e40c6a5a3fb1c8025e0a9f
Abraham Schilling <AbrahamSchilling@gmail.com>
2022-02-05 18:04:56 +0100

    Add unit test for OAuth scopes

---
+++ test/server/github/GitHubAuthProvider.test.ts
@@ -0,0 +1,26 @@
+import { GitHubAuthProvider } from "src/server/github/AuthProvider"
+import { ParsedPluginConfig } from "src/server/plugin/Config"
+
+describe("GitHubAuthProvider", () => {
+  describe("getLoginUrl", () => {
+    it("initiates OAuth flow with the correct scopes", () => {
+      function shouldIncludeRepoScope(repositoryAccess: boolean) {
+        const config: Partial<ParsedPluginConfig> = { repositoryAccess }
+        const provider = new GitHubAuthProvider(config as ParsedPluginConfig)
+        const loginUrl = provider.getLoginUrl("callbackUrl")
+        const orgAndRepoScope = "&scope=" + encodeURIComponent("read:org,repo")
+        const orgScope = "&scope=" + encodeURIComponent("read:org")
+
+        if (repositoryAccess) {
+          expect(loginUrl).toContain(orgAndRepoScope)
+        } else {
+          expect(loginUrl).not.toContain("repo")
+          expect(loginUrl).toContain(orgScope)
+        }
+      }
+
+      shouldIncludeRepoScope(true)
+      shouldIncludeRepoScope(false)
+    })
+  })
+})
