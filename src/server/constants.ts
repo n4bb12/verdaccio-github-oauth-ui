24965e3da04ce4b45532e924a2336e45948a25dc
Abraham Schilling <6810177+n4bb12@users.noreply.github.com.>
2026-01-20 12:41:08 +0100

    build: upgrade parcel and adjust runtime dirname resolution

--- src/server/constants.ts
+++ src/server/constants.ts
@@ -1,4 +1,10 @@
 import { pluginKey } from "../constants"
 
-export const publicRoot = __dirname + "/../client"
+// Parcel will replace `__dirname` with the directory of the source file during
+// the build. Using `eval` prevents this static analysis and substitution,
+// allowing us to access the `__dirname` of the compiled output file at runtime
+// to locate the adjacent client assets.
+const fileDirectory = eval("__dirname")
+
+export const publicRoot = fileDirectory + "/../client"
 export const staticPath = "/-/static/" + pluginKey
